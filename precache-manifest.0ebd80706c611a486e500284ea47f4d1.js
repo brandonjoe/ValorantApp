83f231733c9c53a3430923ffe431a63fa6bb6088
BrandonJoe <brandon.joe42@yahoo.com>
2020-05-15 19:32:35 -0700

    Updates

---
+++ precache-manifest.0ebd80706c611a486e500284ea47f4d1.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "face3439765b9c4aa8ae1a479c736a9e",
+    "url": "/ValorantApp/index.html"
+  },
+  {
+    "revision": "c685986e65ba4c54e346",
+    "url": "/ValorantApp/static/css/main.056f1724.chunk.css"
+  },
+  {
+    "revision": "3a84e77bc0213d9e480f",
+    "url": "/ValorantApp/static/js/2.3d625943.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ValorantApp/static/js/2.3d625943.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c685986e65ba4c54e346",
+    "url": "/ValorantApp/static/js/main.d5b1a44a.chunk.js"
+  },
+  {
+    "revision": "ad04c6e302217efca6c9",
+    "url": "/ValorantApp/static/js/runtime-main.b24765df.js"
+  },
+  {
+    "revision": "7e389c5e310dc537b083e0e25ea6eab5",
+    "url": "/ValorantApp/static/media/DrukWide.7e389c5e.ttf"
+  },
+  {
+    "revision": "561f38b1f4570de0fb8a39d691ab058c",
+    "url": "/ValorantApp/static/media/Tungsten-Bold.561f38b1.ttf"
+  },
+  {
+    "revision": "6eb0f68429b89c1a30f84f1a11875bd7",
+    "url": "/ValorantApp/static/media/ValorantFont.6eb0f684.ttf"
+  },
+  {
+    "revision": "a3359ef4389742811a7ddff2e12249bc",
+    "url": "/ValorantApp/static/media/bind_background.a3359ef4.jpg"
+  },
+  {
+    "revision": "36ad8f197ea5908f9322945a1cd1845a",
+    "url": "/ValorantApp/static/media/intro_background.36ad8f19.jpg"
+  },
+  {
+    "revision": "228298c9d1782a5ad65ee2784246901d",
+    "url": "/ValorantApp/static/media/intro_sova.228298c9.png"
+  },
+  {
+    "revision": "35501dd0598f4ba4380dc12d6c262d98",
+    "url": "/ValorantApp/static/media/maps_background.35501dd0.jpg"
+  }
+]);

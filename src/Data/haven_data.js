05b941723f9f4e857d75235af7ce57b1876ef596
BrandonJoe <brandon.joe42@yahoo.com>
2020-06-18 12:40:35 -0700

    updated content

--- src/Data/haven_data.js
+++ src/Data/haven_data.js
@@ -1,12 +1,12 @@
 export const haven_data = [
 	{
 		id: 1,
-		title: 'HAVEN 1',
+		title: 'Garden from A site',
 		isActive: true,
 		difficulty: 'Easy',
 		side: 'Attacking',
 		essential: true,
-		video: '429164524',
+		video: '430192154',
 		image1: 'https://i.imgur.com/uCi8MgD.png',
 		image2: '',
 		info:
@@ -14,12 +14,14 @@ export const haven_data = [
 	},
 	{
 		id: 2,
-		title: 'HAVEN 2 bolt',
+		title: 'Mid window from B site (A connector side)',
 		isActive: false,
-		difficulty: 'Hard',
+		difficulty: 'Medium',
 		side: 'Defending',
 		essential: false,
-		video: '429164524',
+		video: '429846569',
+		bounces: 1,
+		charges: 2,
 		image1: 'https://i.imgur.com/p0CWF5T.png',
 		image2: 'https://i.imgur.com/UkPTPi7.png',
 		info:
@@ -27,12 +29,14 @@ export const haven_data = [
 	},
 	{
 		id: 3,
-		title: 'HAVEN 3 bolt',
+		title: 'Mid window from B site (gong)',
 		isActive: false,
-		difficulty: 'Medium',
+		difficulty: 'Easy',
 		side: 'Defending',
-		essential: false,
-		video: 'https://player.vimeo.com/video/418358446',
+		essential: true,
+		video: '429846506',
+		bounce: 0,
+		charges: 1,
 		image1: 'https://i.imgur.com/ItcMZBM.png',
 		image2: 'https://i.imgur.com/quhSNu0.png',
 		info:
@@ -40,12 +44,14 @@ export const haven_data = [
 	},
 	{
 		id: 4,
-		title: 'HAVEN 4 site bolt',
+		title: 'C site from C lobby',
 		isActive: false,
-		difficulty: 'Easy',
+		difficulty: 'Medium',
 		side: 'neither',
-		essential: true,
-		video: 'https://player.vimeo.com/video/419342441',
+		essential: false,
+		video: '429846196',
+		bounce: '1',
+		charges: '2',
 		image1: 'https://i.imgur.com/ItcMZBM.png',
 		image2: '',
 		map: 'https://i.imgur.com/IKazxbr.jpg',
@@ -54,70 +60,83 @@ export const haven_data = [
 	},
 	{
 		id: 5,
-		title: 'HAVEN 5 bolt',
+		title: 'C lobby from C site',
 		isActive: false,
-		difficulty: 'Medium',
+		difficulty: 'Easy',
 		side: 'Attacking',
-		essential: false,
-		video: 'https://player.vimeo.com/video/418734606',
+		essential: true,
+		video: '429846285',
+		bounce: 0,
+		charges: 1,
 		map: 'https://i.imgur.com/IKazxbr.jpg',
 		info:
 			'Laborum ex in aute nostrud reprehenderit fugiat sunt ex laborum sit. Velit nulla ut est magna sunt elit laboris deserunt. Laborum do deserunt elit adipisicing. Est quis consectetur pariatur tempor id reprehenderit officia ea adipisicing excepteur ea. In excepteur tempor est labore mollit officia nostrud labore sunt exercitation deserunt tempor ipsum. Ad cillum eu eiusmod duis aliquip qui qui qui nulla sunt cupidatat pariatur.'
 	},
 	{
 		id: 6,
-		title: 'HAVEN 6 bolt',
+		title: 'C long/C Lobby from C site',
 		isActive: false,
 		difficulty: 'Medium',
 		side: 'Attacking',
 		essential: false,
-		video: 'https://player.vimeo.com/video/418358446',
+		video: '429846421',
 		map: 'https://i.imgur.com/IKazxbr.jpg',
 		info:
 			'Consectetur pariatur eiusmod proident amet do cillum officia cupidatat ea Lorem ea. Officia irure eu voluptate enim culpa eiusmod sit Lorem anim exercitation incididunt eu sint do. Fugiat sint enim nulla ut. Consequat aliqua do labore sit dolore Lorem esse occaecat non nisi fugiat non. Nostrud reprehenderit laboris proident est et quis amet dolor adipisicing laboris minim. Laboris aliqua officia magna enim non cupidatat.'
 	},
 	{
 		id: 7,
-		title: 'HAVEN 7 site bolt',
+		title: 'Grass from C link',
 		isActive: false,
-		difficulty: 'Easy',
-		side: 'neither',
+		difficulty: 'Medium',
+		side: 'Defending',
 		essential: true,
+		video: "429846382",
+		bounce: 1,
+		charge: 1,
 		info:
 			'Dolor et proident anim mollit occaecat proident ipsum irure ullamco enim. Ullamco magna cupidatat veniam pariatur et excepteur occaecat enim cupidatat sunt eu. Do elit est nostrud ex aliqua adipisicing deserunt irure sint. Incididunt dolor anim consequat ad sit mollit eiusmod anim voluptate aliquip sunt. Consequat ipsum eiusmod aliqua deserunt magna et magna cillum anim sunt cillum dolor dolore. Laborum labore non proident officia ipsum aliqua consequat ipsum veniam quis cillum eiusmod. Eu laboris elit id consequat incididunt velit.'
 	},
 	{
 		id: 8,
-		title: 'HAVEN 8 bolt',
+		title: 'B site from garden',
 		isActive: false,
 		difficulty: 'Hard',
 		side: 'Attacking',
 		essential: false,
+		video: "429845924",
+		bounce: 2,
+		charges: 2,
 		info:
 			'Laborum ex in aute nostrud reprehenderit fugiat sunt ex laborum sit. Velit nulla ut est magna sunt elit laboris deserunt. Laborum do deserunt elit adipisicing. Est quis consectetur pariatur tempor id reprehenderit officia ea adipisicing excepteur ea. In excepteur tempor est labore mollit officia nostrud labore sunt exercitation deserunt tempor ipsum. Ad cillum eu eiusmod duis aliquip qui qui qui nulla sunt cupidatat pariatur.'
 	},
 	{
 		id: 9,
-		title: 'HAVEN 9 bolt',
+		title: 'A lobby/garden from A tunnel/A site',
 		isActive: false,
-		difficulty: 'Medium',
+		difficulty: 'Hard',
 		side: 'Defending',
-		essential: true,
+		essential: false,
+		video: "429845998",
+		bounce: 0,
+		charges: 2,
 		info:
 			'Consectetur pariatur eiusmod proident amet do cillum officia cupidatat ea Lorem ea. Officia irure eu voluptate enim culpa eiusmod sit Lorem anim exercitation incididunt eu sint do. Fugiat sint enim nulla ut. Consequat aliqua do labore sit dolore Lorem esse occaecat non nisi fugiat non. Nostrud reprehenderit laboris proident est et quis amet dolor adipisicing laboris minim. Laboris aliqua officia magna enim non cupidatat.'
 	}
 ];
 export const haven_detail = {
 	id: 1,
-	title: 'HAVEN 1',
-	isActive: false,
-	difficulty: 'Easy',
+	title: 'Garden from A site',
+	isActive: true,
+	difficulty: 'Medium',
 	side: 'Attacking',
 	essential: true,
-	video: '',
-	image1: 'https://i.imgur.com/uCi8MgD.png',
-	image2: '',
-	map: 'https://i.imgur.com/IKazxbr.jpg',
+	video: '430192154',
+	bounces: 0,
+	charges: 1,
+	image1: 'https://i.imgur.com/e8So83V.jpg',
+	image2: 'https://i.imgur.com/tS3QV6c.png',
+	map: 'https://i.imgur.com/BtBpdy6.jpg',
 	info:
-		'Dolor et proident anim mollit occaecat proident ipsum irure ullamco enim. Ullamco magna cupidatat veniam pariatur et excepteur occaecat enim cupidatat sunt eu. Do elit est nostrud ex aliqua adipisicing deserunt irure sint. Incididunt dolor anim consequat ad sit mollit eiusmod anim voluptate aliquip sunt. Consequat ipsum eiusmod aliqua deserunt magna et magna cillum anim sunt cillum dolor dolore. Laborum labore non proident officia ipsum aliqua consequat ipsum veniam quis cillum eiusmod. Eu laboris elit id consequat incididunt velit.'
+		"There are plenty of hookah darts out there but this one fully clears Hookah 100% of the time. Line up your character until the white space disappears in the first image. Then set 2 bounces and fully charge slightly to the right of the left side of the crate. It doesn't matter how high or below you aim it, but try to get it in the middle like shown in the 2nd image. It doesn't need to be fully percise, but practice before hand. This is a great dart for attacking because after a few rounds, they won't even bother holding hookah. If they continue to hold hookah, you can slow push and be ready to wall bang the couch. It's relatively safe since you're darting from the left hand side of B short. Careful red spot, it's an off angle and that's the only spot that won't be scanned. I've yet to see someone hold from there, but be careful."
 };
